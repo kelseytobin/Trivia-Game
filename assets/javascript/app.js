723305a28e267a3cfb6ea588c94f226f6bd831b0
Kelsey Tobin <etobinkelsey@gmail.com>
2018-12-20 17:04:23 -0700

    latest update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ var counter = 0;
 var potterQuestions = [{
     question: "What plant does Harry eat to breathe underwater in the Triwizard Tournament?",
     choices: ["Lakeweed", "Gillyweed", "Breatheme", "Mandrake"],
-    correct: 1
+    correct: "Gillyweed",
   },
   {
     question: "Who guards the Gryffindor common room?",
@@ -18,58 +18,47 @@ var potterQuestions = [{
       "The Fat Lady",
       "Nearly Headless Nick"
     ],
-    correct: 2
+    correct: "Moaning Myrtle",
   },
   {
     questions: "What did Hagrid name his dragon?",
     choices: ["Buckbeak", "Fluffy", "Hedwig", "Norbert"],
-    correct: 3
+    correct: "Norbert",
   },
   {
     question: "What enchanted object can transport someone to another location?",
     choices: ["Portkey", "A Fireplace", "A Boot", "An Elevator"],
-    correct: 0
+    correct: "Portkey",
   },
   {
     question: "How many Deathly Hallows are there?",
     choices: ["7", "5", "3", "4"],
-    correct: 2
+    correct: "3",
   }
 ];
 
-//display trivia function
 function displayTrivia() {
   startTimer();
   $("#question").html(potterQuestions[counter].question);
   question++;
   var choicesArr = potterQuestions[counter].choices;
   for (var i = 0; i < choicesArr.length; i++) {
-    // $("<div>").addClass(className).attr("id", className);
-    // choices.addClass("button");
-    // choicesText = potterQuestions[counter]("choices" + (i +1));
-    
-   
     var className = (choicesArr[i]).toString();
-    var button = $("<button>").addClass(className).attr("id", className);
-    button.addClass("button");
-    button.text(choicesArr[i]);
-    $("#choices").append(button);
-    button.click(function () {
-      input = $(this).attr("id");
-      console.log(input[0].innerHTML)
-      console.log($(this).attr("id", i));
+    var btn = $("<button>");
+    //adds class to the button
+    btn.addClass("btn-answer", className);
+    //adds a data attribute
+    btn.attr("id", potterQuestions[counter].choices[i]);
+    //labels button
+    btn.text(choicesArr[i]);
+    //adds button to the button view div
+    $("#answers").append(btn);
+    $("#answers").click(function () {
+      compare();
     })
   }
 };
 
- // button.attr("id", (i + 1));
-
-                  // var className = (i + 1).toString();
-                  // var answerDiv = $("$<div>").addClass(className).attr("value", className);
-//                answerDiv.addClass("btn d-block button");
-//                answerText = currentQuestion["choice" + (i + 1)];
-//                answerDiv.text(answerText);
-//                $("#answerCol").append(answerDiv);
 
 function startTimer() {
   timeLeft = 31;
@@ -92,14 +81,17 @@ function updateHTML(secs, count) {
   }
 };
 
+//compare user input again correct answer
 function compare(input) {
-  if (input === potterQuestions[counter].correct) {
-    $("#correct").html("Correct! The answer is " + potterQuestions[counter].correct);
-    counter++;
-  } else {
-    $("#incorrect").html("Nope! The answer is " + potterQuestions[counter].correct);
-    counter++;
-  }
+  input = $(this).attr("id");
+  console.log(input);
+  // var correctAnswer = potterQuestions[counter].correct;
+  // if (input === correctAnswer) {
+  //   console.log("correct");
+  // } else {
+  //   console.log("wrong");
+  // }
+
 };
 
 
@@ -109,8 +101,6 @@ $(document).ready(function () {
     $("#btn-start").hide();
 
     displayTrivia();
-
-    // compare();
 
 
 
